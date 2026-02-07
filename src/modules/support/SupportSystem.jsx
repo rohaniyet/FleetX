9ea887d1739e78ba3d1bd9372d5ee8c7cb89506d
rohaniyet <rohaniyet@gmail.com>
2026-02-08 02:06:18 +0500

    SupportSystem.jsx

---
+++ src/modules/support/SupportSystem.jsx
@@ -0,0 +1,7 @@
+// Integrated support system
+export const SUPPORT_CHANNELS = {
+  WHATSAPP: 'https://wa.me/923454680844',
+  EMAIL: 'support@fleetx.com',
+  PHONE: '0345-4680844',
+  TICKET_SYSTEM: '/support/tickets'
+}
